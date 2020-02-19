e7991b649d7d6701b5105b6652d659ffcdf306e9
Lucas Almeida <lucaspaesalmeida@hotmail.com>
2020-02-18 21:28:54 -0300

    added code format

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { createEditor } from "slate";
+import React, { useCallback, useMemo, useState } from "react";
+import { createEditor, Editor, Transforms } from "slate";
+import { CodeElement, DefaultElement } from "../components";
 
 import { Slate, Editable, withReact } from "slate-react";
 
@@ -12,9 +13,38 @@ const TextEditor = () => {
     }
   ]);
 
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
+  const onKeyDown = (event, editor) => {
+    if (event.ctrlKey && event.key === "'") {
+      event.preventDefault();
+      const [match] = Editor.nodes(editor, {
+        match: n => n.type === "code"
+      });
+
+      Transforms.setNodes(
+        editor,
+        { type: match ? "paragraph" : "code" },
+        { match: n => Editor.isBlock(editor, n) }
+      );
+    }
+  };
+
   return (
     <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-      <Editable />
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={event => {
+          onKeyDown(event, editor);
+        }}
+      />
     </Slate>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 import TextEditor from "./TextEditor";
+import BoldMark from "./BoldMark";
+import CodeElement from "./CodeElement";
+import DefaultElement from "./DefaultElement";
 
-export { TextEditor };
+export { TextEditor, BoldMark, CodeElement, DefaultElement };
